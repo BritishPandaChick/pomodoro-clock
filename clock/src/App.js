838e25b8e9307b336215cf4d32072de40a20a6f9
BritishPandaChick <sbartley1080@gmail.com>
2022-07-11 00:16:30 -0400

    Start adding div tags for app. Fix errors

--- clock/src/App.js
+++ clock/src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
 
-const App = () => {
-  <div>Hello</div>
+function App() {
+  return (
+    <div className="App container">
+      <div className="row">
+        <div className="break-setting">
+          <div id="break-label"></div>
+          <div id="break-increment"></div>
+          <div id="break-length"></div>
+          <div id="break-decrement"></div>
+        </div>
+
+        <div className="session-setting">
+          <div id="session-label"></div>
+          <div id="session-increment"></div>
+          <div id="session-length"></div>
+          <div id="session-decrement"></div>
+        </div>
+      </div>
+
+      <div className="timer">
+        <div id="timer-label"></div>
+        <div id="time-left"></div>
+
+        <div className="row">
+          <div id="start_stop"></div>
+          <div id="reset"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
