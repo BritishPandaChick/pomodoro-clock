89bc79c35d9e565fca9d09d48292aafb570e7546
BritishPandaChick <sbartley1080@gmail.com>
2022-07-12 01:51:39 -0400

    Start adding JS code

--- README.md
+++ README.md
@@ -11,10 +11,10 @@ freeCodeCamp Frontend Libraries project. 25 + 5 clock or Pomodoro Clock.
 Clone or download zip file. Go to your terminal and cd into calculator directory. Run ```npm start``` to start the app.
 
 ## Contributing
-This repository is based on code from the create-react-app and Florin Pop's 25 + 5 tutorial. Therefore, I will not accept pull requests.
+This repository is based on code from the create-react-app and Landon Schlangen's 25 + 5 tutorial. Therefore, I will not accept pull requests.
 
 ## Acknowledgements
 * Dylan Israel
-* Florin Pop
+* Landon Schlangen
 * freeCodeCamp
 * React
--- clock/src/App.js
+++ clock/src/App.js
@@ -1,34 +1,42 @@
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [displayTime, setDisplayTime] = React.useState(25 + 60);
+  const [breakTime, setBreakTime] = React.useState(5 + 60);
+
+  // Format time function to get minutes and seconds
+  const formatTime = (time) => {
+    let minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+    return (minutes < 10 ? "0" + minutes : minutes) + ":" + seconds < 10 ? "0" + seconds : seconds);
+  };
+
   return (
-    <div className="App container">
-      <div className="row">
-        <div className="break-setting">
-          <div id="break-label"></div>
-          <div id="break-increment"></div>
-          <div id="break-length"></div>
-          <div id="break-decrement"></div>
-        </div>
-
-        <div className="session-setting">
-          <div id="session-label"></div>
-          <div id="session-increment"></div>
-          <div id="session-length"></div>
-          <div id="session-decrement"></div>
-        </div>
-      </div>
-
-      <div className="timer">
-        <div id="timer-label"></div>
-        <div id="time-left"></div>
-
-        <div className="row">
-          <div id="start_stop"></div>
-          <div id="reset"></div>
-        </div>
-      </div>
+    <div>
+      <Length
+        title={"break length"}
+        changeTime={null}
+        type={"break"}
+        time={breakTime}
+        formatTime={formatTime}
+      />
+      <h1>{formatTime(displayTime)}</h1>
     </div>
+  );
+}
+
+//Second component to figure out length
+function Length({ title, changeTime, type, time, formatTime }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <div className="time-sets">
+        <button onClick={() => changeTime{-60, type}}>-</button>
+      <h3>{formatTime(time)}</h3>
+      <button>+</button>
+    </div>
+    </div >
   );
 }
 
